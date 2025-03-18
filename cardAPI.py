6af917d91e600b7ba6edca8e691080841e0c13af
ebaucom22 <elijahwbaucom@gmail.com>
2025-03-17 23:06:32 -0400

    Create the KD-Tree to use in the API

--- cardAPI.py
+++ cardAPI.py
@@ -1,12 +1,17 @@
-import json
 from flask import Flask, jsonify, request
 from webscraper import getAllCards
 import random
+import treeKD
 
 app = Flask(__name__)
+
+
+# TODO: Remove this build and create the tree through put methods instead
 card_dict = getAllCards()
-# TODO: Store cards by attributes using a trie or other data structure for faster search
-# TODO: Only return the first 10 results for a search  lexicographically if there are too many results and allow for a page number in the request
+# Store cards by attributes using a K-D tree for faster searching
+tree = treeKD.TreeKD(card_dict)
+
+# TODO: Return the first 10 results for a search lexicographically if there are too many results and allow for a page number in the request
 
 # Get all cards in the database
 @app.route('/cards', methods=['GET'])
@@ -22,14 +27,14 @@ def get_card(card_id):
     else:
         return jsonify({"error": "Card not found"}), 404
 
-# Search cards by attribute
-@app.route('/cards/attribute/<string:attribute>', methods=['GET'])
-def get_cards_by_attribute(attribute):
-    attribute = attribute.lower()
-    cards = [card for card in card_dict.values() if card.get('Attribute', '').lower() == attribute]
-    return jsonify(cards)
+# Search cards by attribute (Deprecated and incorrect)
+# @app.route('/cards/attribute/<string:attribute>', methods=['GET'])
+# def get_cards_by_attribute(attribute):
+#     attribute = attribute.lower()
+#     cards = [card for card in card_dict.values() if card.get('Attribute', '').lower() == attribute]
+#     return jsonify(cards)
 
-# Search for a card by multiple attributes
+# Search for A SINGLE card by multiple attributes
 @app.route('/cards/search/<string:attributes>', methods=['GET'])
 def search_cards(attributes):
     attributes = attributes.lower().split(',')
@@ -39,13 +44,15 @@ def search_cards(attributes):
             results.append(card)
     return jsonify(results)
 
+# TODO: Search for multiple cards by multiple attributes
+
 # Return a random card from the database
 @app.route('/cards/random', methods=['GET'])
 def get_random_card():
     card = random.choice(list(card_dict.values()))
     return jsonify(card)
 
-# TODO: Create an autocomplete function using the trie I will create by name
+# TODO: Create an autocomplete function using the trie I will create for search by name
 
 if __name__ == '__main__':
    app.run(port=5000)
